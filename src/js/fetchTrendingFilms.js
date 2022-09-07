2457248021e6edca7ba3ecf1dd49ea517a49b3a0
KutsevEA <kucev.ea@gmail.com>
2022-09-07 15:45:18 +0300

    Fetch With Paginatio
    Добавил в Config Базовый URL
    Написал и экспортировал Функцию для фэтча ТрендФильмов с пагинацией
    Имортировал функци в index.js

---
+++ src/js/fetchTrendingFilms.js
@@ -0,0 +1,25 @@
+// Импортируем настройки для запроса на сервер
+import config from './config';
+
+// Экспортируем функцию поиска фильмов в тренде 
+export { fetchTrendingFilms };
+
+// Функция запроса фильмов в тренде с пагинацией - Функция ожидает в 
+// переменную "page" номер страници которую нужно загрузить. Функция возвращает масив из 20 обьектов. 
+async function fetchTrendingFilms(page) {
+  try {
+    const response = await fetch(
+      `${config.base_url}/trending/all/day?page=${page}&api_key=${config.api_key}`
+    );
+    const films = await response.json();
+    return films;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+//  !!!!!!!!!! Перед сдачей проекта удалить этот кусок -  
+// Консолим результат работы функции для теста
+let page = 1
+fetchTrendingFilms(page).then(films => console.log(films));
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 import config from "./config";
+import { fetchTrendingFilms } from './fetchTrendingFilms';
 
+
+//  !!!!!!!!!! Перед сдачей проекта удалить этот кусок -  
+// Консолим результат работы функции для теста
+let page = 1;
+fetchTrendingFilms(page).then(films => console.log(films));
