9282b03cc00cf938595aa4ab9621d18015e379b0
Nick Zheliba <nikolaj.jeliba@gmail.com>
2022-09-08 22:39:49 +0300

    Moved options to the new file.

--- src/js/search-movie.js
+++ src/js/search-movie.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import debounce from 'lodash.debounce';
 import config from "./config";
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
+import options from './options-notiflix'
 
 const KEY = config.api_key;
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 const POSTER_URL = 'https://image.tmdb.org/t/p/'
-const POSTER_SIZE = 'w200'
+const POSTER_SIZE = 'w400'
 
 export default async function fetchMovie(query, page = 1) {
     const params = {
@@ -24,8 +25,8 @@ export default async function fetchMovie(query, page = 1) {
 }
 
 const inputSearch = document.querySelector('.search-form__input');
-// Тут треба замінити "galleryEl" на той елемент-контейнер, де буде рендер фільмів по запиту. 
-// const galleryEl = document.querySelector('.gallery')
+// Тут треба підставити той елемент, де буде рендер фільмів по запиту. 
+// const listFilms = document.querySelector('.films')
 inputSearch.addEventListener('input', debounce(searchMovie, 1000));
 
 let page = 1;
@@ -39,53 +40,26 @@ async function searchMovie(e) {
     console.log(e.target.value);
 
   if (query === '') {
-    Notiflix.Notify.info('Please enter a movie name to search.', option);
+Notify.info('Please enter a movie name to search.', options);
     return;
   }
 
   if (query === repeatQuery) {
-    Notiflix.Notify.info('Please enter a new movie name to search.', option);
+Notify.warning('Please enter another movie name to search.', options);
     return;
   }
 
   await fetchMovie(query, page).then(movie => {
   console.log(movie)
       if (movie.data.results.length === 0) {
-    //   galleryEl.innerHTML = '';
-      Notiflix.Notify.failure(
-          'Search result not successful. Enter the correct movie name and try again please.', option);
+      // listFilms.innerHTML = '';
+Notify.failure('Search result not successful. Enter the correct movie name and try again please.', options);
       return;
     }
 
-    Notiflix.Notify.success(`According to your request, we found ${movie.data.total_results} movies.`, option);
-    // galleryEl.innerHTML = '';
-    //   createGallaryMarkup(movie.data.results);
+Notify.success(`According to your request, we found ${movie.data.total_results} movies.`, options);
+    // listFilms.innerHTML = '';
+      // createGallaryMoviesMarkup(movie.data.results); Змінити назву функції на функцію з маркапом.
     });
 };
 
-const option = {
-    width: '393px',
-    position: 'center-top',
-    distance: '162px',
-    zindex: 4001,
-    fontFamily: 'Roboto',
-    fontSize: '14px',
-    // font-weight: '700',
-    useIcon: false,
-            
-    success: {
-        background: 'transparent',
-        textColor: '#04BF00',
-    },
-    failure: {
-        background: 'transparent',
-        textColor: '#FF001B',
-    },
-    warning: {
-        background: 'transparent',
-        textColor: '#FF6B08',
-    },
-    info: {
-        background: 'transparent',
-        textColor: '#08C4FF',
-    }};
