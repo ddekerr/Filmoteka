05574244dcd8077245fbe466acf6c216cd994950
Darya Hudzovska <1bestboost@gmail.com>
2022-09-08 19:27:54 +0300

    test

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -23,14 +23,49 @@ var opts = {
   position: 'absolute', // Element positioning
 };
 
-
-
-var target = document.querySelector('.wrapper');
-
+// var target = document.querySelector('.wrapper');
 var spinner = new Spinner(opts).spin(target);
+
+// console.log(spinner.el)
+
 
 // // Чтобы вручную вставить спиннер в DOM, вы можете вызвать метод spin()
 // var spinner = new Spinner().spin();
-// target.appendChild(spinner);
+// target.appendChild(spinner.el);
 
 // // Чтобы спрятать спиннер, вы можете вызвать метод stop()
+
+
+// const body = document.querySelector('.wrapper')
+// const loadBtn = document.createElement('button')
+// loadBtn.classList.add('.load')
+// loadBtn.style.backgroundColor = "teal";
+// loadBtn.style.padding = "24px";
+
+
+// console.log(loadBtn.style);
+// body.appendChild(loadBtn);
+
+// loadBtn.addEventListener('click',  addSpinner())
+// console.log(loadBtn)
+// loadBtn.insertAdjacentHTML("afterbegin", addSpinner())
+
+
+// function addSpinner() {
+        
+ 
+    // var target = document.querySelector('.wrapper');
+// var spinner = new Spinner(opts).spin(loadBtn);
+// loadBtn.insertAdjacentHTML("afterbegin", spinner.el);
+// console.log(spinner.el)
+
+// }
+
+// function test() {
+//     setTimeout(function addSpinner(){
+//        var spinner = new Spinner().spin(); 
+
+   
+//     }, 1000);
+// }
+// test()
