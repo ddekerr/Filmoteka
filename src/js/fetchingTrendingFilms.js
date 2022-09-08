007a074f7145ffd32b254f8ce2fc7db595a2eb19
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-09-08 11:32:29 +0300

    create config for request and import to index

--- src/js/fetchingTrendingFilms.js
+++ src/js/fetchingTrendingFilms.js
@@ -10,17 +10,17 @@ export { fetchTrendingFilms };
 // Функция запроса фильмов в тренде с пагинацией - Функция ожидает в
 // переменную "page" номер страници которую нужно загрузить. Функция возвращает масив из 20 обьектов.
 async function fetchTrendingFilms(page) {
+  const config = {
+    baseURL: base_url,
+    params: {
+      page: page,
+      api_key: api_key
+    }
+  }
   try {
-    const films = await axios.get(
-      `${base_url}/trending/movie/week?page=${page}&api_key=${api_key}`
-    );
+    const films = await axios.get('/trending/movie/week', config);
     return films;
   } catch (error) {
     console.log(error.message);
   }
 }
-
-//  !!!!!!!!!! Перед сдачей проекта удалить этот кусок -
-// Консолим результат работы функции для теста
-let page = 1;
-fetchTrendingFilms(page).then(films => console.log(films));
