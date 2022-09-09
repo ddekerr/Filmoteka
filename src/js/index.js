a6467a470f315dfdcea12a328b1952079533002e
Darya Hudzovska <1bestboost@gmail.com>
2022-09-09 21:46:38 +0300

    change marks

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 import { gallery } from './refs';
 // import config from './config';
-
 import modal from './modal';
-
 // import './library-buttons';
 import { pagination } from './pagination';
-
 import {
   fetchTrendingMovies,
   fetchMoviesByQuery,
@@ -13,25 +10,21 @@ import {
   genersForFilmCard,
 } from './filmoteka';
 import { createFilmsGallery } from './markups';
-
 /**
  * Default request when page opening
  * if everything fine render films gallery
  */
 const pagin = pagination();
 const page = pagin.getCurrentPage();
-
 fetchTrendingMovies(page).then(data => {
   const total = data.total_results;
   pagin.reset(total);
   const markup = createFilmsGallery(data.results);
   gallery.innerHTML = markup;
 });
-
 pagin.on('beforeMove', event => {
   // получаем номер активной страницы на кнопках
   const currentPage = event.page;
-
   // получаем фильмы согласно страницы
   fetchTrendingMovies(currentPage).then(data => {
     const markup = createFilmsGallery(data.results);
