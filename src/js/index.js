33ad6f2e430f8bc77dc3b130fc18fdb524e5e5d2
AnnaSidiakina <fruktik@gmail.com>
2022-09-09 17:37:23 +0200

    render
    добавила рендер по кнопке, сделала, чтобы подтягивалось нужное количество фильмов с бегэнда для пагинации

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,29 @@ import modal from './modal';
 
 import { fetchTrendingFilms } from './fetchingTrendingFilms';
 // import './library-buttons';
-import { paganation } from './pagination';
+import { pagination } from './pagination';
+import { createFilmsGallery } from './markUpFilmsGallery';
+
+const pagin = pagination();
+const page = pagin.getCurrentPage();
+
+fetchTrendingFilms(page)
+  .then(result => ({
+    items: result.data.results,
+    total: result.data.total_results,
+  }))
+  .then(({ items, total }) => {
+    createFilmsGallery(items);
+    pagin.reset(total);
+  });
+
+pagin.on('beforeMove', event => {
+  // получаем номер активной страницы на кнопках
+  const currentPage = event.page;
+  // console.log(currentPage);
+
+  // получаем фильмы согласно страницы и рендерим их
+  fetchTrendingFilms(currentPage).then(res => {
+    createFilmsGallery(res.data.results);
+  });
+});
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,46 +1,89 @@
 import Pagination from 'tui-pagination';
 // import 'tui-pagination/dist/tui-pagination.css';
 import { pag } from './refs';
+// import { fetchTrendingFilms } from './ftf';
 import { fetchTrendingFilms } from './fetchingTrendingFilms';
+import { createFilmsGallery } from './markUpFilmsGallery';
 
-// const pag = document.querySelector('.tui-pagination');
+export function pagination(params = {}) {
+  const options = {
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: 1,
+    centerAlign: true,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+      page: `<a href="#" class="tui-page-btn">{{page}}</a>`,
+      currentPage: `<strong class="tui-page-btn tui-is-selected">{{page}}</strong>`,
+      moveButton:
+        `<a href="#" class="tui-page-btn tui-{{type}}">` +
+        `<span class="tui-ico-{{type}}">{{type}}</span>` +
+        `</a>`,
+      disabledMoveButton:
+        `<span class="tui-page-btn tui-is-disabled tui-{{type}}">` +
+        `<span class="tui-ico-{{type}}">{{type}}</span>` +
+        `</span>`,
+      moreButton:
+        `<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">` +
+        `<span class="tui-ico-ellip">...</span>` +
+        `</a>`,
+    },
+  };
+  return new Pagination(pag, { ...options, ...params });
+}
 
-const options = {
-  totalItems: 20000,
-  itemsPerPage: 20,
-  visiblePages: 5,
-  page: 1,
-  centerAlign: true,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: `<a href="#" class="tui-page-btn">{{page}}</a>`,
-    currentPage: `<strong class="tui-page-btn tui-is-selected">{{page}}</strong>`,
-    moveButton:
-      `<a href="#" class="tui-page-btn tui-{{type}}">` +
-      `<span class="tui-ico-{{type}}">{{type}}</span>` +
-      `</a>`,
-    disabledMoveButton:
-      `<span class="tui-page-btn tui-is-disabled tui-{{type}}">` +
-      `<span class="tui-ico-{{type}}">{{type}}</span>` +
-      `</span>`,
-    moreButton:
-      `<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">` +
-      `<span class="tui-ico-ellip">...</span>` +
-      `</a>`,
-  },
-};
-const paganation = new Pagination(pag, options);
+// const paganation = new Pagination(pag, { ...options });
 
-console.log(paganation);
+// fetchTrendingFilms(1).then(result => {
+//   const total = result.data.total_results;
+//   paganation.setTotalItems(total);
+//   console.log(total);
+//   return total;
+// });
+
+// .then(data => ({
+//   itemsFilm: data.results,
+//   total: 20000,
+// }))
+// .then(({ itemsFilm, total }) => {
+//   console.log(itemsFilm);
+//   console.log(total);
+// });
+
+// console.log(paganation);
 // получаем первую страницу
-const firstPage = fetchTrendingFilms(options.page);
-console.log(firstPage);
-paganation.on('beforeMove', event => {
-  // получаем номер активной страницы на кнопках
-  const currentPage = event.page;
-  console.log(currentPage);
-  // получаем фильмы согласно страницы
-  const movies = fetchTrendingFilms(currentPage);
-  console.log(movies);
-});
+// const firstPage = fetchTrendingFilms(options.page);
+// console.log(firstPage);
+
+// paganation.on('beforeMove', event => {
+//   paganation.setTotalItems(20000);
+//   // получаем номер активной страницы на кнопках
+//   const currentPage = event.page;
+//   // console.log(currentPage);
+
+//   // получаем фильмы согласно страницы и рендерим их
+//   fetchTrendingFilms(currentPage).then(res => {
+//     createFilmsGallery(res.data.results);
+//   });
+// });
+
+// let totalItemsFromServer;
+
+// const init = async total => {
+//   if (total === undefined && !totalItemsFromServer) {
+//     totalItemsFromServer = await fetchTrendingFilms(page);
+//   }
+
+//   if (total === undefined) {
+//     total = totalItemsFromServer.total_results;
+//   }
+
+//   paganation.setTotalItems(total);
+//   // paganation.reset();
+// };
+
+// init();
+// export default {
+//   reset: init,
+// };
