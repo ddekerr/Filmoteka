b921489c153ca8c47bd7bacbcb3bd1659e53632f
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-09-09 17:17:59 +0300

    pileup all func to fetching API and create markup file to set all markup functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
-import './refs';
-import config from './config';
+// import './refs';
+// import config from './config';
 
-import modal from './modal';
+// import modal from './modal';
 
-import { fetchTrendingFilms } from './fetchingTrendingFilms';
+// import { fetchTrendingFilms } from './fetchingTrendingFilms';
 // import './library-buttons';
-import { paganation } from './pagination';
+// import { paganation } from './pagination';
+
+import {fetchTrendingMovies, fetchMoviesByQuery, fetchMovieByID} from './filmoteka';
+
+const a = fetchTrendingMovies(1)
+const b = fetchMoviesByQuery('term')
+const c = fetchMovieByID(100)
+
+console.log(a, b, c)
