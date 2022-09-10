dd0fcaa373e2593174e9e9010eba1343a3ec30f6
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-09-10 18:27:15 +0300

    Markup change

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,5 @@ export default {
   base_url: 'https://api.themoviedb.org/3',
   postersUrl: 'https://image.tmdb.org/t/p/',
   postersSize: 'w1280',
+  altPosterUrl: 'https://www.vaureal.fr/sites/vaureal/files/styles/_site_contenu_image_principale/public/image/2022-03/Solidarit%C3%A9%20ukraine.jpg?itok=Gcqq2tD3'
 };
--- src/js/markups.js
+++ src/js/markups.js
@@ -7,28 +7,12 @@ import {genersForFilmCard} from './getGenres'
  */
 export function createFilmsGallery(items) {
   const markupСard = items.map(item => {
-    if (item.poster_path === null) {
-      console.log('Here no poster_path', item);
-      return `
-    <li class="film">
-      <a class="film__link link" href="">
-        <div class="film__image-container">
-          <img class="film__image" src="https://www.vaureal.fr/sites/vaureal/files/styles/_site_contenu_image_principale/public/image/2022-03/Solidarit%C3%A9%20ukraine.jpg?itok=Gcqq2tD3" alt="${item.original_title}" load="lazy"/>
-        </div>
-        <h2 class="film__name title">${item.title}</h2>
-        <div class="film__description">
-          <p class="film__genre">${genersForFilmCard(item.genre_ids)}</p>
-          <p class="film__year">${item.release_date.slice(0, 4)}</p>
-          <p class="film__rating">${item.vote_average.toFixed(1)}</p>
-        </div>
-      </a>
-    </li>
-  `}
+    const src = item.poster_path === null ? config.altPosterUrl : config.postersUrl + config.postersSize + item.poster_path;
     return `
     <li class="film">
       <a class="film__link link" href="">
         <div class="film__image-container">
-          <img class="film__image" src="${config.postersUrl + config.postersSize + item.poster_path}" alt="${item.original_title}" load="lazy"/>
+          <img class="film__image" src="${src}" alt="${item.original_title}" load="lazy"/>
         </div>
         <h2 class="film__name title">${item.title}</h2>
         <div class="film__description">
@@ -40,5 +24,6 @@ export function createFilmsGallery(items) {
     </li>
   `}
   ).join('');
+
   return markupСard;
 }
