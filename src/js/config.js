64f78a9bbebf7ce829d7b75262fc0c6ae9cff36d
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-09-10 21:31:24 +0300

    change poster size

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@ export default {
   api_key: 'fd7e9d42e4eb94adcf7c367528854213',
   base_url: 'https://api.themoviedb.org/3',
   postersUrl: 'https://image.tmdb.org/t/p/',
-  postersSize: 'w1280',
+  postersSize: 'w500',
   altPosterUrl: 'https://www.vaureal.fr/sites/vaureal/files/styles/_site_contenu_image_principale/public/image/2022-03/Solidarit%C3%A9%20ukraine.jpg?itok=Gcqq2tD3'
 };
