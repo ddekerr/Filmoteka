a02185edc6164f64a4a49bffead12bf4f55a2d6b
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-09-11 19:06:15 +0300

    markup fix

--- src/js/markups.js
+++ src/js/markups.js
@@ -50,56 +50,57 @@ export function createFilmsGallery(items) {
  * @param {Object} item film object
  * @returns {String} markup
  */
-export function createModalFilm(item) {
+ export function createModalFilm(item) {
   const src = item.poster_path === null ? config.altPosterUrl : config.postersUrl + config.postersSize + item.poster_path;
-  return `
-    <button class="modal__close" modal-close>
+    return `
+    <div class="modal__movie">
+      <button class="modal__close" modal-close>
       <svg class="modal__btn" width="20px" height="20px">
-        <use href="../images/icons.svg#icon-close" fill="black"></use>
+        <use  href="./images/icons.svg#icon-arrow-left" fill="black"></use>
       </svg>
-    </button>
-    <div class="modal__img">
-      <img src="${src}" alt="${item.original_title}" />
-    </div>
-    <div class="modal-info">
-      <h2 class="modal-info__header title">${item.title}</h2>
-      <table class="modal-info-table">
-        <tbody>
-          <tr class="modal-info__row">
-            <td><p class="modal-info__attribute">Vote / Votes</p></td>
-            <td>
-              <p><span class="film__rating modal-info__rating">${item.vote_average}</span> / ${item.vote_count}</p>
-            </td>
-          </tr>
-          <tr class="modal-info__row">
-            <td><p>Popularity</p></td>
-            <td><p>${item.popularity}</p></td>
-          </tr>
-          <tr class="modal-info__row">
-            <td><p>Original Title</p></td>
-            <td><p>${item.original_title}</p></td>
-          </tr>
-          <tr class="modal-info__row">
-            <td><p>Genre</p></td>
-            <td><p>${item.genres.map(item => item.name).join(', ')}</p></td>
-          </tr>
-        </tbody>
-      </table>
-
-      <h3 class="modal-info__about-title">About</h3>
-      <p class="modal-info__about">
-        ${item.overview}
-      </p>
-      <ul class="modal-info__buttons-list">
-         <li class="movie-data__button-item">
-           <button type="button" data-btn="watched" class="modal-info__button button active">
-            add to Watched
-           </button>
-         </li>
-         <li class="modal-info__button-item">
-           <button type="button" data-btn="queue" class="modal-info__button button">add to queue</button>
-         </li>
-       </ul>
+      </button>
+      <div class="modal__img">
+        <img src="${src}" alt="${item.original_title}" />
+      </div>
+      <div class="modal-info">
+        <h2 class="modal-info__header title">${item.title}</h2>
+        <table class="modal-info-table">
+          <tbody>
+            <tr class="modal-info__row">
+              <td><p class="modal-info__attribute">Vote / Votes</p></td>
+              <td>
+                <p><span class="film__rating modal-info__rating">${item.vote_average}</span> / ${item.vote_count}</p>
+              </td>
+            </tr>
+            <tr class="modal-info__row">
+              <td><p>Popularity</p></td>
+              <td><p>${item.popularity}</p></td>
+            </tr>
+            <tr class="modal-info__row">
+              <td><p>Original Title</p></td>
+              <td><p>${item.original_title}</p></td>
+            </tr>
+            <tr class="modal-info__row">
+              <td><p>Genre</p></td>
+              <td><p>${item.genres.map(item => item.name).join(', ')}</p></td>
+            </tr>
+          </tbody>
+        </table>
+        <h3 class="modal-info__about-title">About</h3>
+        <p class="modal-info__about">
+          ${item.overview}
+        </p>
+        <ul class="modal-info__buttons-list">
+          <li class="movie-data__button-item">
+            <button type="button" data-btn="watched" class="modal-info__button button active">
+              add to Watched
+            </button>
+          </li>
+          <li class="modal-info__button-item">
+            <button type="button" data-btn="queue" class="modal-info__button button">add to queue</button>
+          </li>
+        </ul>
+      </div>
     </div>
   `
 }
