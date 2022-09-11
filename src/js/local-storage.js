5f715c9a83f472619001c03192c438e248ec8618
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-09-11 23:11:27 +0300

    add comment to locale storage file

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -3,6 +3,10 @@ import {fetchMovieByID} from './filmoteka';
 import './refs';
 
 
+/**
+ * Call function to save local film
+ * @param {event} e 
+ */
 export async function saveFilm(e) {
   const filmId = e.target.dataset.id;
   const storageKey = e.target.dataset.btn;
@@ -10,13 +14,21 @@ export async function saveFilm(e) {
   addFilmToLocaleStorage(film, storageKey);
 }
 
+/**
+ * Call function to remove local film
+ * @param {event} e 
+ */
 export function removeFilm(e) {
   const filmId = e.target.dataset.id;
   const storageKey = e.target.dataset.btn;
   removeFilmFromLocalrStorage(filmId, storageKey);
 }
 
-
+/**
+ * Add new film to locale storage if that film doesn`t exist
+ * @param {Object} newFilm film to adding to locale storage
+ * @param {String} localeStorageKey key of locale storage which film will be adding
+ */
 function addFilmToLocaleStorage(newFilm, localeStorageKey) {
   const localStData = JSON.parse(localStorage.getItem(localeStorageKey)) || [];
   const isFilmHere = localStData.find(film => film.id === newFilm.id) || false;
@@ -26,7 +38,11 @@ function addFilmToLocaleStorage(newFilm, localeStorageKey) {
   }
 }
 
-
+/**
+ * Remove film from locale storage by id
+ * @param {Integer} id id by which the movie will be deleted
+ * @param {String} localeStorageKey key of locale storage which film will be adding
+ */
 export function removeFilmFromLocalrStorage(id, localeStorageKey){
   const localStData = JSON.parse(localStorage.getItem(localeStorageKey)) || false;
   if(localStData) {
