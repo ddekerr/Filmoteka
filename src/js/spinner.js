cdd10d8f791b75ab85dbf62125dc7896545f6931
Darya Hudzovska <1bestboost@gmail.com>
2022-09-12 17:24:28 +0300

    spinner place

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -14,7 +14,7 @@ var opts = {
   direction: 1, // 1: clockwise, -1: counterclockwise
   color: '#ff6b01', // CSS color or array of colors
   fadeColor: 'transparent', // CSS color or array of colors
-  top: '50%', // Top position relative to parent
+  top: '170%', // Top position relative to parent
   left: '50%', // Left position relative to parent
   shadow: '0 0 1px transparent', // Box-shadow for the lines
   zIndex: 2000000000, // The z-index (defaults to 2e9)
