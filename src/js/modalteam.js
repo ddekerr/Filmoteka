d0b8030a29baa2aa6acbb360813282899151d6b2
KutsevEA <kucev.ea@gmail.com>
2022-09-12 18:21:48 +0300

    Критические ошибки валидатора

--- src/js/modalteam.js
+++ src/js/modalteam.js
@@ -1,42 +1,41 @@
 (() => {
-   const refs = {
-      openModalTeamButton: document.querySelector(".footer__link"),
-      closeModalTeamButton: document.querySelector("[modalteam-close]"),
-      modalTeam: document.querySelector("[modalteam]"),
-      backdropTeam: document.querySelector('.backdropteam'),
-   };
+  const refs = {
+    openModalTeamButton: document.querySelector('.footer__link'),
+    closeModalTeamButton: document.querySelector('.modalteam__close'),
+    modalTeam: document.querySelector('.backdropteam.backdrop'),
+    backdropTeam: document.querySelector('.backdropteam'),
+  };
 
-   if (refs.openModalTeamButton) {
-      refs.openModalTeamButton.addEventListener("click", openModalTeam);
-   }
+  if (refs.openModalTeamButton) {
+    refs.openModalTeamButton.addEventListener('click', openModalTeam);
+  }
 
-   if (refs.closeModalTeamButton) {
-      refs.closeModalTeamButton.addEventListener("click", closeModalTeam);
-   }
+  if (refs.closeModalTeamButton) {
+    refs.closeModalTeamButton.addEventListener('click', closeModalTeam);
+  }
 
-   function openModalTeam() {
-      refs.modalTeam.classList.toggle("is-open");
-      document.body.style.overflow = 'hidden';
-      document.addEventListener('keydown', offModalForEscape);
-      document.addEventListener('click', offModalBackdropTeam)
-   };
+  function openModalTeam() {
+    refs.modalTeam.classList.toggle('is-open');
+    document.body.style.overflow = 'hidden';
+    document.addEventListener('keydown', offModalForEscape);
+    document.addEventListener('click', offModalBackdropTeam);
+  }
 
-   function closeModalTeam() {
-      refs.modalTeam.classList.toggle("is-open");
-      document.body.style.overflow = 'overlay';
-      document.removeEventListener('keydown', offModalForEscape);
-      document.removeEventListener('click', offModalBackdropTeam)
+  function closeModalTeam() {
+    refs.modalTeam.classList.toggle('is-open');
+    document.body.style.overflow = 'overlay';
+    document.removeEventListener('keydown', offModalForEscape);
+    document.removeEventListener('click', offModalBackdropTeam);
+  }
 
-   };
-
-   function offModalForEscape(event) {
-      if (event.key === 'Escape') {
-         closeModalTeam();
-      }
-   }
-   function offModalBackdropTeam(event) {
-      if (event.target === refs.backdropTeam) {
-         closeModalTeam();
-      }
-   }
+  function offModalForEscape(event) {
+    if (event.key === 'Escape') {
+      closeModalTeam();
+    }
+  }
+  function offModalBackdropTeam(event) {
+    if (event.target === refs.backdropTeam) {
+      closeModalTeam();
+    }
+  }
 })();
