9866246a141bfd02c017919e38998671ca3c8a7d
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-09-13 01:45:19 +0100

    fix

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -4,30 +4,30 @@ const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
 
-const setupUI = user => {
-  if (user) {
-    // account info
-    db.collection('users')
-      .doc(user.uid)
-      .get()
-      .then(doc => {
-        const html = `
-        <div>Logged in as ${user.email}</div>
-        <div>${doc.data().bio}</div>
-      `;
-        accountDetails.innerHTML = html;
-      });
-    // toggle user UI elements
-    loggedInLinks.forEach(item => (item.style.display = 'block'));
-    loggedOutLinks.forEach(item => (item.style.display = 'none'));
-  } else {
-    // clear account info
-    accountDetails.innerHTML = '';
-    // toggle user elements
-    loggedInLinks.forEach(item => (item.style.display = 'none'));
-    loggedOutLinks.forEach(item => (item.style.display = 'block'));
-  }
-};
+// const setupUI = user => {
+//   if (user) {
+//     // account info
+//     db.collection('users')
+//       .doc(user.uid)
+//       .get()
+//       .then(doc => {
+//         const html = `
+//         <div>Logged in as ${user.email}</div>
+//         <div>${doc.data().bio}</div>
+//       `;
+//         accountDetails.innerHTML = html;
+//       });
+//     // toggle user UI elements
+//     loggedInLinks.forEach(item => (item.style.display = 'block'));
+//     loggedOutLinks.forEach(item => (item.style.display = 'none'));
+//   } else {
+//     // clear account info
+//     accountDetails.innerHTML = '';
+//     // toggle user elements
+//     loggedInLinks.forEach(item => (item.style.display = 'none'));
+//     loggedOutLinks.forEach(item => (item.style.display = 'block'));
+//   }
+// };
 
 // setup guides
 const setupGuides = data => {
